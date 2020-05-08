c59ce4b8e6bfea7122a0e9dcfcddc282699d5aca
aayushya <aayushya1@gmail.com>
2020-05-08 20:06:42 +0530

    testing standard ip method

--- src/main/java/com/devops/abv/App.java
+++ src/main/java/com/devops/abv/App.java
@@ -1,16 +1,21 @@
 package com.devops.abv;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws Exception
     {
-        Integer n1 = 40;
-        Integer n2 = 50;
-
+   //     Integer n1 = 40;
+     //   Integer n2 = 50;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer n1 = Integer.parseInt(br.readLine());
+        Integer n2 = Integer.parseInt(br.readLine());
         System.out.println("Output for the addition is : "+addition(n1,n2));
 
         System.out.println("Output for the subtraction is : "+subtraction(n1,n2));
