5333874d1b6fa5354b3dd0471b85b3ffc1b7b921
aayushya <aayushya1@gmail.com>
2020-05-08 20:41:40 +0530

    Multiplication method added

--- src/main/java/com/devops/abv/App.java
+++ src/main/java/com/devops/abv/App.java
@@ -11,17 +11,20 @@ public class App
 {
     public static void main( String[] args )throws Exception
     {
-   //     Integer n1 = 40;
-     //   Integer n2 = 50;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer n1 = Integer.parseInt(br.readLine());
-        Integer n2 = Integer.parseInt(br.readLine());
+        Integer n1 = 40;
+        Integer n2 = 50;
+
+
         System.out.println("Output for the addition is : "+addition(n1,n2));
 
         System.out.println("Output for the subtraction is : "+subtraction(n1,n2));
+
+        System.out.println("Output for the Multiplication is : "+multiplication(n1,n2));
     }
 
     public static Integer addition(Integer a,Integer b){return a+b;}
 
     public static Integer subtraction(Integer a,Integer b){return  b-a;}
+
+    public static Integer multiplication(Integer a,Integer b){return a*b;}
 }
